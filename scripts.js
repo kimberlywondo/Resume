2c46da2658a2e80880d8649dc3776b214ba10be0
Kimberly Wondoloski <kimberly.wondoloski@gmail.com>
2017-11-03 19:55:37 -0400

    have functional scroll-snap resume template

--- scripts.js
+++ scripts.js
@@ -1,15 +1,29 @@
-$(function(){
-    var shrinkHeader = 100;
-     $(window).scroll(function() {
-       var scroll = getCurrentScroll();
-         if ( scroll >= shrinkHeader ) {
-              $('.header').addClass('shrink');
-           }
-           else {
-               $('.header').removeClass('shrink');
-           }
-     });
-   function getCurrentScroll() {
-       return window.pageYOffset;
-       }
-   });
+$(document).ready(function(){
+    // Add smooth scrolling to all links
+    $("a").on('click', function(event) {
+  
+      if (this.hash !== "") {
+        event.preventDefault();
+  
+        // Store hash
+        var hash = this.hash;
+  
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 800, function(){
+     
+          window.location.hash = hash;
+        });
+      } // End if
+    });
+  });
+
+// $('a[href^="#"]').on('click', function(event) {
+//     var target = $(this.getAttribute('href'));
+//     if( target.length ) {
+//         event.preventDefault();
+//         $('html, body').stop().animate({
+//             scrollTop: target.offset().top
+//         }, 3000);
+//     }
+// });
